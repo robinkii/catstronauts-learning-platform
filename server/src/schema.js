df8ef475870db24bb5c29bb1637afd216f8757d1
Robin Kelsick II <robinkelsick@gmail.com>
2023-11-15 22:23:54 -0500

    defined query and author types

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,15 +4,35 @@ const gql = require('graphql-tag');
 
 const typeDefs = gql`
   # This is where the schema definitions go.
+  type Query {
+    "Need to get tracks to display for the homepage"
+    tracksForHome: [Track!]
+  }
 
   "A track is a group of modules that teach a specific topic"
   type Track {
+    "pk"
     id: ID!
+    "name of the track"
     title: String!
+    "author of the track"
     author: Author!
+    "picture that the track will display"
     thumbnail: String
+    "the length of the track"
     length: int
+    "how many modules in the track"
     modulesCount: int
+  }
+
+  "This shows the author of a specific module or track"
+  type Author {
+    "pk"
+    id: ID!
+    "author first and last name"
+    name: String!
+    "picture of the author"
+    photo: String
   }
 `;
 
