49da54fcd68d99922313e2fdcffc6c81041fe2f2
Robin <robinkelsick@gmail.com>
2023-11-16 11:55:43 -0500

    finished with backend enough to test on frontend.

--- server/src/index.js
+++ server/src/index.js
@@ -6,9 +6,40 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const typeDefs = require('./schema');
+const { addMocksToSchema } = require('@graphql-tools/mock');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 async function startServer() {
-  const server = new ApolloServer({ typeDefs });
+  // Defines mock data values for a specific track.
+  const mocks = {
+    // By default only two Tracks would be returned, we need to return 6 tracks as an array.
+    Query: () => ({
+      tracksForHome: () => [...new Array(6)],
+    }),
+    Track: () => ({
+      id: () => 'track_01',
+      title: () => 'Astro Kitty, Space Explorer',
+      author: () => {
+        return {
+          name: 'Grumpy Cat',
+          photo:
+            'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+        };
+      },
+      thumbnail: () =>
+        'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+      length: () => 1210,
+      modulesCount: () => 6,
+    }),
+  };
+
+  const server = new ApolloServer({
+    // seed the mock data. Asking the server to populate all the data with random data.
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
   const { url } = await startStandaloneServer(server);
 
   console.log(
