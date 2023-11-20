f029cc5c1e78637a481ccac86a73612beb01db22
Robin <robinkelsick@gmail.com>
2023-11-20 11:51:52 -0500

    added gql query to display info

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.systemMessage}`;
+
+  const TRACKS = gql`
+    query GetTracks {
+      tracksForHome {
+        id
+        title
+        thumbnail
+        length
+        modulesCount
+        author {
+          id
+          name
+          photo
+        }
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return <Layout grid>{JSON.stringify(data)} </Layout>;
 };
 
 export default Tracks;
